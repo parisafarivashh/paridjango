085fd4f960d651b61054357fd29f12edb4748d99
parisa <parisafarivash@gmail.com>
2020-02-11 09:10:12 +0200

    create models

--- buy/models.py
+++ buy/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Buy(models.Model):
+  name = models.CharField(max_length=100)
+  price = models.FloatField()
+  stock = models.IntegerField()
+  image = models.ImageField(upload_to='image/')
+
+def __str__(self):
+  return self.name  
