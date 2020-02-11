0f2eeba51ad30792536f61489a2a2d8473bfcf93
parisa <parisafarivash@gmail.com>
2020-02-11 09:10:31 +0200

    create admin

--- buy/admin.py
+++ buy/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Buy
 
-# Register your models here.
+class BuyAdmin(admin.ModelAdmin):
+  list_display =('name','price','stock')
+
+admin.site.register(Buy,BuyAdmin)
